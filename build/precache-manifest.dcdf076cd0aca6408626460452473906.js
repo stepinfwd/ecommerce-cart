0066fb7fc2d4cad22bf826b6651922359a6931c5
stepinfwd <stepinfwd@gmail.com>
2020-11-11 16:52:01 +0530

    deploy

---
+++ build/precache-manifest.dcdf076cd0aca6408626460452473906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3587e8d06c9095ca18108822e73d806e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc673b333a105bbed84a",
+    "url": "/static/css/main.021112d5.chunk.css"
+  },
+  {
+    "revision": "a616232645d72db27bc4",
+    "url": "/static/js/2.94e16cd6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.94e16cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc673b333a105bbed84a",
+    "url": "/static/js/main.a87079ad.chunk.js"
+  },
+  {
+    "revision": "601060052b73bcd6a0ac",
+    "url": "/static/js/runtime-main.da240beb.js"
+  }
+]);
